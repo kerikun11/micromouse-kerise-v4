8f8982c5bda1f4ccc4049192d31c2790750322c7
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-02-10 23:40:08 +0900

    update shape

--- firmware/lib/utils/slalom_shapes.h
+++ firmware/lib/utils/slalom_shapes.h
@@ -4,17 +4,17 @@
 
 using namespace ctrl;
 
-static const auto SS_SL90 = ctrl::slalom::Shape(ctrl::Position(45, 45, 1.5708), ctrl::Position(44.0134, 44.0073, 1.5708), 0.986565, 0.992722, 265.761); //< T: 0.287448 [s]
-static const auto SS_SR90 = ctrl::slalom::Shape(ctrl::Position(45, -45, -1.5708), ctrl::Position(44.0134, -44.0073, -1.5708), 0.986565, 0.992722, 265.761); //< T: 0.287448 [s]
-static const auto SS_FL45 = ctrl::slalom::Shape(ctrl::Position(90, 45, 0.785398), ctrl::Position(74.8347, 31.0004, 0.785398), 1.16565, 19.7985, 425.272); //< T: 0.248641 [s]
-static const auto SS_FR45 = ctrl::slalom::Shape(ctrl::Position(90, -45, -0.785398), ctrl::Position(74.8347, -31.0004, -0.785398), 1.16565, 19.7985, 425.272); //< T: 0.248641 [s]
-static const auto SS_FL90 = ctrl::slalom::Shape(ctrl::Position(90, 90, 1.5708), ctrl::Position(70.0213, 70.0043, 1.5708), 19.9787, 19.9957, 422.846); //< T: 0.374537 [s]
-static const auto SS_FR90 = ctrl::slalom::Shape(ctrl::Position(90, -90, -1.5708), ctrl::Position(70.0213, -70.0043, -1.5708), 19.9787, 19.9957, 422.846); //< T: 0.374537 [s]
-static const auto SS_FL135 = ctrl::slalom::Shape(ctrl::Position(45, 90, 2.35619), ctrl::Position(33.1881, 80.0014, 2.35619), 21.8106, 14.1402, 353.771); //< T: 0.464955 [s]
-static const auto SS_FR135 = ctrl::slalom::Shape(ctrl::Position(45, -90, -2.35619), ctrl::Position(33.1881, -80.0014, -2.35619), 21.8106, 14.1402, 353.771); //< T: 0.464955 [s]
-static const auto SS_FL180 = ctrl::slalom::Shape(ctrl::Position(0, 90, 3.14159), ctrl::Position(0, 90, 3.14159), 24, 24, 412.408); //< T: 0.563056 [s]
-static const auto SS_FR180 = ctrl::slalom::Shape(ctrl::Position(0, -90, -3.14159), ctrl::Position(0, -90, -3.14159), 24, 24, 412.408); //< T: 0.563056 [s]
-static const auto SS_FLV90 = ctrl::slalom::Shape(ctrl::Position(63.6396, 63.6396, 1.5708), ctrl::Position(50.0153, 50.0078, 1.5708), 13.6243, 13.6318, 302.004); //< T: 0.370251 [s]
-static const auto SS_FRV90 = ctrl::slalom::Shape(ctrl::Position(63.6396, -63.6396, -1.5708), ctrl::Position(50.0153, -50.0078, -1.5708), 13.6243, 13.6318, 302.004); //< T: 0.370251 [s]
-static const auto SS_FLS90 = ctrl::slalom::Shape(ctrl::Position(45, 45, 1.5708), ctrl::Position(44.0134, 44.0073, 1.5708), 0.986565, 0.992722, 265.761); //< T: 0.287448 [s]
-static const auto SS_FRS90 = ctrl::slalom::Shape(ctrl::Position(45, -45, -1.5708), ctrl::Position(44.0134, -44.0073, -1.5708), 0.986565, 0.992722, 265.761); //< T: 0.287448 [s]
+static const auto SS_SL90 =     ctrl::slalom::Shape(ctrl::Position(       45,        45,    1.5708), ctrl::Position(  44.0068,    44.001,    1.5708),  0.993221,  0.999035,    265.76); /*< T:  0.287496 [s] */
+static const auto SS_SR90 =     ctrl::slalom::Shape(ctrl::Position(       45,       -45,   -1.5708), ctrl::Position(  44.0068,   -44.001,   -1.5708),  0.993221,  0.999035,    265.76); /*< T:  0.287496 [s] */
+static const auto SS_FL45 =     ctrl::slalom::Shape(ctrl::Position(       90,        45,  0.785398), ctrl::Position(   74.835,        31,  0.785398),   1.16494,    19.799,   425.285); /*< T:  0.248639 [s] */
+static const auto SS_FR45 =     ctrl::slalom::Shape(ctrl::Position(       90,       -45, -0.785398), ctrl::Position(   74.835,       -31, -0.785398),   1.16494,    19.799,   425.285); /*< T:  0.248639 [s] */
+static const auto SS_FL90 =     ctrl::slalom::Shape(ctrl::Position(       90,        90,    1.5708), ctrl::Position(  70.0179,   69.9971,    1.5708),   19.9821,   20.0029,   422.868); /*< T:  0.374557 [s] */
+static const auto SS_FR90 =     ctrl::slalom::Shape(ctrl::Position(       90,       -90,   -1.5708), ctrl::Position(  70.0179,  -69.9971,   -1.5708),   19.9821,   20.0029,   422.868); /*< T:  0.374557 [s] */
+static const auto SS_FL135 =    ctrl::slalom::Shape(ctrl::Position(       45,        90,   2.35619), ctrl::Position(  33.1882,   80.0001,   2.35619),   21.8117,    14.142,   353.773); /*< T:  0.464962 [s] */
+static const auto SS_FR135 =    ctrl::slalom::Shape(ctrl::Position(       45,       -90,  -2.35619), ctrl::Position(  33.1882,  -80.0001,  -2.35619),   21.8117,    14.142,   353.773); /*< T:  0.464962 [s] */
+static const auto SS_FL180 =    ctrl::slalom::Shape(ctrl::Position(        0,        90,   3.14159), ctrl::Position(        0,        90,   3.14159),        24,        24,   412.408); /*< T:  0.563056 [s] */
+static const auto SS_FR180 =    ctrl::slalom::Shape(ctrl::Position(        0,       -90,  -3.14159), ctrl::Position(        0,       -90,  -3.14159),        24,        24,   412.408); /*< T:  0.563056 [s] */
+static const auto SS_FLV90 =    ctrl::slalom::Shape(ctrl::Position(  63.6396,   63.6396,    1.5708), ctrl::Position(  48.0118,   48.0008,    1.5708),   15.6278,   15.6388,   289.946); /*< T:  0.387836 [s] */
+static const auto SS_FRV90 =    ctrl::slalom::Shape(ctrl::Position(  63.6396,  -63.6396,   -1.5708), ctrl::Position(  48.0118,  -48.0008,   -1.5708),   15.6278,   15.6388,   289.946); /*< T:  0.387836 [s] */
+static const auto SS_FLS90 =    ctrl::slalom::Shape(ctrl::Position(       45,        45,    1.5708), ctrl::Position(  44.0068,    44.001,    1.5708),  0.993221,  0.999035,    265.76); /*< T:  0.287496 [s] */
+static const auto SS_FRS90 =    ctrl::slalom::Shape(ctrl::Position(       45,       -45,   -1.5708), ctrl::Position(  44.0068,   -44.001,   -1.5708),  0.993221,  0.999035,    265.76); /*< T:  0.287496 [s] */
